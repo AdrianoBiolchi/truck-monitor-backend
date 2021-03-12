a54b8da87535a57ad3e3295b7acc7f6106cb844a
Adriano Biolchi <adrianobiolchi@outlook.com>
2021-03-11 22:55:04 -0300

    First Commit Truck Monitor

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const { response } = require('express')
+const express = require('express')
+const routes = require('./routes')
+
+const app = express()
+
+app.use(routes)
+
+
+
+var Port = process.env.PORT || 3333
+
+app.listen(Port, () => {
+  console.log('🚀 Back-end Truck Monitor started', +Port)
+})
+
